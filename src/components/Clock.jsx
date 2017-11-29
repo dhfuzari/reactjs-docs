ad4bf1a23266eb0520eaae6594471b49d9c8a230
Daniel H. Fuzari <dhfuzari@gmail.com>
2017-11-29 15:41:50 -0200

    feat(clock): passisng arguments to event handlers

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -5,7 +5,10 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.clickMe = this.clickMe.bind(this);
-        this.state = { date: new Date() };
+        this.state = { 
+            date: new Date(),
+            isToogleOn: false 
+        };
     }
 
     componentDidMount() {
@@ -19,9 +22,12 @@ class Clock extends React.Component {
         clearInterval(this.timerID);
     }
 
-    clickMe(e) {
+    clickMe(e, str) {
         e.preventDefault();
-        console.log('This link was clicked!');
+        this.setState((prevState) => ({
+            isToogleOn: !prevState.isToogleOn
+        }));
+        console.log(e, str);        
     }
 
     tick() {
@@ -35,7 +41,7 @@ class Clock extends React.Component {
             <div>
                 <h2>It is <FormatDate date={this.state.date} /></h2>
                 <p>
-                    <a href="#" onClick={this.clickMe}>Click me!</a>
+                    <a href="#" onClick={(e) => {this.clickMe(e, 'TESTE AB')}}>Toogle button - {this.state.isToogleOn.toString()}</a>
                 </p>
             </div>
         );
