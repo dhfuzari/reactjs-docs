9a5c8d339a0edcd9c70d986a92229e69055c943c
Daniel H. Fuzari <dhfuzari@gmail.com>
2017-11-30 16:08:27 -0200

    feat(WarningBanner): prevent component from rendering

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import Welcome from './components/Welcome';
 import LoginControl from './components/LoginControl';
 import Clock from './components/Clock';
 import Mailbox from './components/Mailbox';
+import WarningBanner from './components/WarningBanner';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isLoggedIn: false,
-      unreadMessages: ['React', 'Re: React', 'Re:Re: React']
+    this.state = {      
+      unreadMessages: ['React', 'Re: React', 'Re:Re: React'],
+      showWarning: false
     }
     this.frameworkInfo = {
       name: 'React JS',
       version: '16.1.1',
       author: 'Facebook Inc.'
     };
+
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(
+      prevState => ({showWarning: !prevState.showWarning})
+    );
   }
 
   render() {
@@ -33,6 +42,8 @@ class App extends Component {
           <Clock />             
           <LoginControl />
           <Mailbox unreadMessages={this.state.unreadMessages} />
+          <WarningBanner warn={this.state.showWarning} />
+          <button onClick={this.handleToggleClick}>{this.state.showWarning ? 'Hide' : 'Show'}</button>
         </main>
         <footer id="footer">
           <small className="App-title">Welcome to {this.frameworkInfo.name}</small>
---
+++ src/components/WarningBanner.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+function WarningBanner(props) {
+    if(!props.warn) {
+        return null;
+    }
+    return(
+        <h2 className="warning">Warning!</h2>
+    );
+}
+
+export default WarningBanner;
