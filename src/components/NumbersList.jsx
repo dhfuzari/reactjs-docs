ebf33ec58d509547018b8c0b8d021374e957f6a8
Daniel Henrique Fuzari <dhfuzari@gmail.com>
2017-12-02 16:11:20 -0200

    feat(components/NumbersList): adding keys to list items

--- src/components/NumbersList.jsx
+++ src/components/NumbersList.jsx
@@ -4,7 +4,7 @@ function NumbersList(pros) {
     const numbers = [2, 6, 67, 5, 103];
 
     const listOfNumbers = numbers.map((item, index, arr) => {
-        return <li>{item * item}</li>;
+        return <li  key={index.toString()}>{item * item}</li>;
     })
     return <ul>{listOfNumbers}</ul>;
 }
